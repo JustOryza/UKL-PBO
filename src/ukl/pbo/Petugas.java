54bbf9ce83b7aad6439c9767fa335e25dd42e03f
JustOryza <107356587+JustOryza@users.noreply.github.com>
2022-06-13 11:23:46 +0700

    Update Petugas.java

--- src/ukl/pbo/Petugas.java
+++ src/ukl/pbo/Petugas.java
@@ -1,17 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- *polimor dinamis yaitu yang methodnya sama dengan implements
- */
 package ukl.pbo;
 
 import java.util.ArrayList;
-/**
- *
- * @author MOKLET-2
- */
-
     
     public class Petugas implements User {
 
@@ -19,49 +8,42 @@ import java.util.ArrayList;
     private ArrayList<String> alamat = new ArrayList<String>();
     private ArrayList<String> telepon = new ArrayList<String>();
 
-    public Petugas() {
+    public Petugas() { //set nama petugas dan memasukkan ke dalam arraylist
         this.namaKaryawan.add("Doni");
         this.alamat.add("Malang");
-        this.telepon.add("081111222333");
-    }
+        this.telepon.add("081111222333");}
 
     public int getJmlPetugas() {
-        return this.namaKaryawan.size();
-    }
+        return this.namaKaryawan.size();} //mengembalikan jumlah total petugas perpus
     
     @Override
     public void setNama(String namaKaryawan) {
-        this.namaKaryawan.add(namaKaryawan);
-    }
+        this.namaKaryawan.add(namaKaryawan);} //merubah nama karyawan
 
     @Override
     public void setAlamat(String alamat) {
-        this.alamat.add(alamat);
-    }
+        this.alamat.add(alamat);} //merubah alamat karyawan
 
     @Override
     public void setTelepon(String telepon) {
-        this.telepon.add(telepon);
-    }
+        this.telepon.add(telepon);} //merubah nomor telepon
 
     @Override
     public String getNama(int idKaryawan) {
-        return this.namaKaryawan.get(idKaryawan);
-    }
+        return this.namaKaryawan.get(idKaryawan);} //memngemablikan nilai nama karyawam
 
     @Override
     public String getAlamat(int idKaryawan) {
-        return this.alamat.get(idKaryawan);
-    }
+        return this.alamat.get(idKaryawan); } //mengembalikan alamat karyawan
 
     @Override
     public String getTelepon(int idKaryawan) {
-        return this.telepon.get(idKaryawan);
-    }
+        return this.telepon.get(idKaryawan);} //Mengembalikan nilai nomor telepon karyawan
     
-
 }
-//polimor dinamis yaitu yang methodnya sama dengan implements
-//jika ingin mengakses class lain memakai method
-//override adalah ingin mengakses data dengan menggunakan set get
-//pengertian ovveride sendiri yaitu ingin mengakses data kembali yang ada di user
+/*
+polimor dinamis yaitu yang methodnya sama dengan implements
+jika ingin mengakses class lain memakai method
+override adalah ingin mengakses data dengan menggunakan set get
+pengertian ovveride sendiri yaitu ingin mengakses data kembali yang ada di user
+*/
