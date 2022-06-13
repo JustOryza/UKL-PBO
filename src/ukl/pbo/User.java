69cdda4934571d46179eb8a0403b00f48deb14b5
JustOryza <107356587+JustOryza@users.noreply.github.com>
2022-06-13 10:43:46 +0700

    Update User.java

--- src/ukl/pbo/User.java
+++ src/ukl/pbo/User.java
@@ -1,17 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ukl.pbo;
 
 import java.util.ArrayList;
-/**
- *
- * @author MOKLET-2
- */
-
-    public interface User {
+    public interface User { //set method
     public void setNama(String nama);
     public void setAlamat(String alamat);
     public void setTelepon(String telepon);
